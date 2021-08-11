c721eaf280af6a7cf3c0daeb0fcc96adf5f6e97d
Nicole Soares <nicolesoares98@hotmail.com>
2021-08-11 17:06:43 -0300

    agregar estilos

--- src/componentes/Producto/Producto.jsx
+++ src/componentes/Producto/Producto.jsx
@@ -6,14 +6,20 @@ export default function Producto(props) {
     <div className="contenedor-principal">
       <div className="producto">
         <div className="imagen">
-          <img src={props.imagen} width = "100%"alt="producto" />
+          <img src={props.imagen} width="100%" alt="producto" />
         </div>
         <div className="descripcion">
           <h4>{props.categoria}</h4>
           <h3>{props.nombre}</h3>
         </div>
         <div className="transparente">
-          <h3>{props.precio}</h3>
+          <div className="precio-moneda">
+            <h3 className="precio">{props.precio}</h3>
+            <div className="circulo-precio"></div>
+          </div>
+          <div>
+            <button className="boton-comprar">Redeem now</button>
+          </div>
         </div>
       </div>
     </div>
